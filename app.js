bb1519c9f40c8cd118be3b7ed49d50886e39013a
swapnil lothe <44019530+swapnillothe@users.noreply.github.com>
2019-04-16 15:09:04 +0530

    Connect database to app.

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const Express = require('express');
+const mysql = require('mysql');
+
 const app = new Express();
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    password: process.env.DB_PASSWORD,
+    database: 'book_shop',
+    user: process.env.DB_USER
+});
+
+console.log(process.env);
+connection.connect();
 
 app.use(Express.static('react-app-front-end/build'));
 
-app.get('/getNumber', (req, res) => {
-    res.send(JSON.stringify({ a: 2 }));
+app.get('/getcustomers', (req, res) => {
+    connection.query('select * from customers;', (error, data) => {
+        res.send(JSON.stringify({ data }));
+    });
 });
 
 module.exports = app;
